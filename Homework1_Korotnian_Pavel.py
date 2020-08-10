d54034251132304f4aac20d7d17ca522948bccdc
Pasha <63916313+Zipopasha@users.noreply.github.com>
2020-08-11 00:08:59 +0300

    Add files via upload

---
+++ Homework1_Korotnian_Pavel.py
@@ -0,0 +1,31 @@
+# Homework_1
+# Task_1
+
+a = 10
+b = 30
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b)
+
+# Task_2
+
+x = a < b
+print(x)
+x = a > b
+print(x)
+x = a == b
+print(x)
+x = a != b
+print(x)
+
+# Task_3
+
+str1 = "Hello "
+str2 = "world"
+print(str1 + str2)
+
+# Task_4
+
+new_string = "Hello world!"
+print(len(new_string))
