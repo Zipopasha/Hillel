8226d08d23741f1ac1ae827981b5932bcce7ebda
Pasha <63916313+Zipopasha@users.noreply.github.com>
2020-08-20 23:41:35 +0300

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,9 @@
+def choice(user_input):
+    res = bool
+    #user_input = input('Enter your juy -->')
+    if user_input.lower() == 'y':
+        return True
+    elif user_input.lower() == 'n':
+        return False
+
+
---
+++ main_file.py
@@ -0,0 +1,11 @@
+from lib import choice
+
+user_input_2 = input('olol --> ')
+
+if bool(user_input_2) == True:
+    choice(user_input_2 )
+else:
+    print('idi nahuy')
+
+
+
