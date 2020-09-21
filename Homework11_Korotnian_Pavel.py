e912dc1f43cbebd5f8782aacf73b6b0b6ad6fbbc
Pasha <63916313+Zipopasha@users.noreply.github.com>
2020-09-21 17:43:19 +0300

    Add files via upload

--- Homework11_Korotnian_Pavel.py
+++ Homework11_Korotnian_Pavel.py
@@ -1,27 +1,65 @@
-"""Подключитесь к API НБУ (https://bank.gov.ua/ua/open-data/api-dev),
- получите текущий курс валют и запишите его в TXT-файл в формате
- "дата создания запроса"
+"""Подключитесь к API НБУ (https://bank.gov.ua/ua/open-data/api-dev), получите текущий курс валют
+ и запишите его в TXT-файл в формате - "дата создания запроса"
 1. [название ввалюты 1] to UAH: [значение курса к валюте 1]
 2. [название ввалюты 2] to UAH: [значение курса к валюте 2]
-3. [название ввалюты 3] to UAH: [значение курса к валюте 3]
-...
+3. [название ввалюты 3] to UAH: [значение курса к валюте 3]...
 n. [название ввалюты n] to UAH: [значение курса к валюте n]"""
 
 import requests
 
+# import json
+# import datetime
+
+# url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
+# res = requests.get(url)
+#
+# posts = json.loads(res.content)
+#
+#
+# def file_write(posts):
+#     with open('Currency.txt', 'w') as f:
+#         date = datetime.date.today()
+#         result = ""
+#         i = 0
+#         f.write("Дата создания запроса:  " + date.strftime("%d/%m/%Y\n") + "\n")
+#         for key in (posts):
+#             i += 1
+#             result += f"{i}. {key['txt']} to UAH: {key['rate']}\n"
+#         f.write(result)
+#
+#
+# def check(posts):
+#     try:
+#         file_write(posts)
+#     except:
+#         raise Exception
+#
+#
+# check(posts)
+#
 try:
     response = requests.get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
 except:
-    pass
+    raise Exception
 
-with open('Currency.txt', 'w') as f:
-    i = 1
-    date = response.json()
-    f.write('Дата создания запроса:  ' + (date[0]['exchangedate']) + '\n')
-    for key in response.json():
-        res = '' + str(i) + '. ' + key['txt'] + ' to UAH: ' + str(key['rate']) + '\n'
-        i += 1
-        f.write(res)
+
+def currency_check():
+    if response.status_code != 200:
+        raise Exception
+    # elif response.headers != 'application/json':
+    #     raise Exception
+    else:
+        with open('Currency.txt', 'w') as f:
+            date = response.json()
+            res = ''
+            f.write('Дата создания запроса:  ' + (date[0]['exchangedate']) + '\n')
+            for key in (response.json()):
+                res = '' + str(key['txt'] + ' to UAH: ' + str(key['rate']) + '\n')
+                f.write(str(enumerate(res)))
+            f.write(res)
+
+
+currency_check()
 
 """2. ** Пользователь вводит название валюты и дату, программа возвращает пользователю курс гривны
  к этой валюте за указаную дату используя API НБУ. Формат ввода пользователем данных - на ваше усмотрение. 
---
+++ Homework12_Korotnian_Pavel.py
@@ -0,0 +1,23 @@
+"""Напишите функцию для парсинга номерных знаков автомоблей Украины (стандарты - AА1234BB, 12 123-45AB, a12345BC) с
+помощью регулярных выражений. Функция принимает строку и возвращает None если строка не является номерным знаком.
+Если является номерным знаком - возвращает саму строку. """
+
+import re
+
+auto_numbers = input()
+
+
+def check_automobile_numbers(input_text):
+    tpl = r'[a-zA-Z0-9]{2}[0-9]{4}[A-Z]{2}'
+    regexp = re.compile(tpl)
+    res = regexp.findall(input_text)
+    if not res:
+        return print(None)
+    else:
+        return print(res)
+
+
+check_automobile_numbers(auto_numbers)
+
+
+# 'AA1234BB, 12 123-45AB, a12345BC'
